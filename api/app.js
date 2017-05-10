3d547dbfedfcd932dc9787780200e6d961b5937d
jgarzon <jgarzon@easysol.net>
2017-05-09 22:48:35 -0500

    api mod 3

--- api/app.js
+++ api/app.js
@@ -7,6 +7,9 @@ var app = express();
 
 //cargar rutas
 
+var user_routes =require('./routes/user');
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
@@ -14,6 +17,7 @@ app.use(bodyParser.json());
 
 
 //rutas base
+app.use('/api',user_routes );
 
 app.get('/pruebas',function (req,res) {
     res.status(200).send({message: 'Bienvenido al curso de javascript'});
