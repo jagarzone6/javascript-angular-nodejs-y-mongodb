280bebef1f13f26e506c521e391772307b7e6733
jgarzon <jgarzon@easysol.net>
2017-05-10 09:29:38 -0500

    api login method

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,4 +10,6 @@ api.get('/probando-controlador',UserController.pruebas );
 
 api.post('/register',UserController.saveUser );
 
+api.post('/login',UserController.loginUser );
+
 module.exports = api;
