0e1e25a24181a9206e3c4c351d71590896dff237
jgarzon <jgarzon@easysol.net>
2017-05-09 22:01:34 -0500

    api mod 3

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2',(err,res) => {
     }else {
         console.log("LA base de datos esta corriendo correctamente...");
         app.listen(port,function () {
-            console.log("servidor del api rest de musica eschucahdo en http://localhost:"+ port);
+            console.log("servidor del api rest de musica escuchando en http://localhost:"+ port);
         });
     }
 });
