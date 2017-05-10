cd6613d12670019fa978f2d5b78cfa57e707425d
jgarzon <jgarzon@easysol.net>
2017-05-09 23:00:37 -0500

    api mod 3

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,7 @@
 'use strict'
+var bcrypt = require('bcrypt-nodejs');
+var User = require('../models/user');
+
 
 function pruebas (req,res) {
     res.status(200).send({
@@ -7,6 +10,28 @@ function pruebas (req,res) {
 
 }
 
+function saveUser (req,res) {
+    var user = new User();
+
+    var params = req.body;
+
+    console.log(params);
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+
+    if(params.password){
+        //Encriptar y guardar dato
+
+
+    } else {
+        res.status(500).send({message:'Introduce la contraseña'});
+    }
+}
+
 module.exports = {
-  pruebas
+  pruebas,
+  saveUser
 };
