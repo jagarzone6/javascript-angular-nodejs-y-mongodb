f02d8961f36068e6b9f2108327b7400cc80a877d
jgarzon <jgarzon@easysol.net>
2017-05-11 11:42:40 -0500

    api song controlle

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -154,7 +154,7 @@ function uploadImage(req,res){
 
                 }else {
 
-                    res.status(200).send({user: userUpdated});
+                    res.status(200).send({image: file_name,user: userUpdated});
 
                 }
 
