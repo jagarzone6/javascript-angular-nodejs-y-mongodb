57cb63875456f82ea82f3676ea45a815cef484bd
jgarzon <jgarzon@easysol.net>
2017-05-12 23:47:17 -0500

    client -  login view

---
+++ client/src/app/models/song.ts
@@ -0,0 +1,10 @@
+export class Song{
+  constructor(
+    public number: number,
+    public name: string,
+    public duration: number,
+    public file: string,
+    public album: string
+  ){}
+
+}
