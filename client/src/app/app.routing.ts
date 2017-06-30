72e15b0e42e5e3b88ecd40333ed063ca34f653a6
jgarzon <jgarzon@easysol.net>
2017-06-30 14:27:45 -0500

    client/artist

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -3,10 +3,12 @@ import { Routes,RouterModule } from '@angular/router';
 
 //import user
 import { UserEditComponent } from './components/user-edit.component';
+import { ArtistListComponent } from './components/artist-list.component';
 
 const appRoutes: Routes = [
   {path: "", component: UserEditComponent},
   {path: "mis-datos",component: UserEditComponent},
+  {path: "artists",component: ArtistListComponent},
   {path: "**",component: UserEditComponent}
 ];
 
